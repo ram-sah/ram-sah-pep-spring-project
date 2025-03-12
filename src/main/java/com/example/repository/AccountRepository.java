ae0b8215953c1d6a70a688d8cc2207d6214cb44d
Ram Sah <mohan2036@gmail.com>
2025-03-12 00:34:46 +0000

    configured MessageRepository and MessageService

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -3,9 +3,11 @@ package com.example.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer>{
     Optional<Account> findByUsername(String username);
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,5 +1,16 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
-    
+    private MessageRepository messageRepository;
+
+      @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 }
